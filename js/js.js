865f788599aa6796811ea0e3ecf242a7c0878ed0
AndreaChensq <105483515+AndreaChensq@users.noreply.github.com>
2022-07-21 12:50:39 +0800

    first version of wiki

---
+++ js/js.js
@@ -0,0 +1,43 @@
+const mobileNav = document.getElementById('main-nav-mb')
+const faders = document.querySelectorAll(".fader")
+const uppers = document.querySelectorAll(".upper")
+const lefters = document.querySelectorAll(".lefter")
+const righters = document.querySelectorAll(".righter")
+
+// mb-nav
+function showMobileNav() {
+	mobileNav.style.top = "0";
+}
+
+function hideMobileNav() {
+	mobileNav.style.top = "-1000px";
+}
+// appearOnScroll
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnscroll) {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) {
+			return
+		} else {
+			entry.target.classList.add("show");
+			appearOnScroll.unobserve(entry.target)
+		}
+	})
+}, {
+	rootMargin: "0px 0px -70px 0px"
+})
+
+
+
+// application
+faders.forEach(fader => {
+	appearOnScroll.observe(fader)
+})
+uppers.forEach(upper => {
+	appearOnScroll.observe(upper)
+})
+lefters.forEach(lefter => {
+	appearOnScroll.observe(lefter)
+})
+righters.forEach(righter => {
+	appearOnScroll.observe(righter)
+})
